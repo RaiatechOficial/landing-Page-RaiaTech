2b42c55b319643cf8e708b24acd9c68d0dd0c8c5
Sávio Marques <143422882+Saviommarques@users.noreply.github.com>
2023-11-30 21:19:48 -0300

    RaiaTech

---
+++ JS/funcoes.js
@@ -0,0 +1,54 @@
+//Soma//
+function soma(numero1, numero2){
+    return numero1 + numero2;
+}
+
+
+const resultado = soma(3,4);
+console.log(resultado);
+
+
+//Subtração//
+function sub(numero1, numero2){
+    return numero1 - numero2;
+}
+
+
+const result = sub(7,6);
+console.log(result)
+
+
+//Multiplicação//
+
+
+function mult(numero1, numero2){
+    return numero1 * numero2
+}
+
+
+const resuti = mult(2,8);
+console.log(resuti);
+
+
+//Divisão//
+
+
+function div(numero1, numero2){
+    return numero1 / numero2;
+}
+
+
+const resuto = div(10,5);
+console.log(resuto);
+
+
+//Média//
+
+
+function calcularMedia(numero1, numero2 , numero3, numero4){
+    return (numero1 + numero2 + numero3 + numero4) / 4;
+}
+
+
+const mostrar = calcularMedia(1,4,6,9);
+console.log(mostrar);
